1b06cbf6243513f7a45216343f5dddb54a7abc79
JoA Azria <joa.azria@gmail.com>
2021-04-02 15:07:27 -0300

    users profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+ protected
+
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :email, :password, :company, :country, :activity, :website, :gender, :first_name, :last_name, :phone, :whatsapp, :release, :linkedin, :status, :member, :job])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :email, :password, :company, :country, :activity, :website, :gender, :first_name, :last_name, :phone, :whatsapp, :release, :linkedin, :status, :member, :job])
+
+
+  end
+
+  def after_sign_in_path_for(resource)
+  stored_location_for(resource) || profile_path
+end
+
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,23 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :about_us, :speaker ]
+  # before_action :set_user, only: [:profile, :show, :edit, :update, :destroy]
 
   def home
   end
+  def profile
+    # @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+        # user = Users.find(params[:id])
+        user = current_user
+  end
+
+  private
+
+  #   def set_user
+  #   @user = User.find(params[:id])
+  # end
+  def user_params
+  params.require(:user).permit(:avatar, :id, :email, :password, :company, :country, :activity, :website, :gender, :first_name, :last_name, :phone, :whatsapp, :release, :status, :member)
+  end
 end
+
