d3244909235cb4f69febeea63593aa249a2c3823
Danniel Tung <dwftung@gmail.com>
2021-04-02 13:40:36 -0300

    started mailing process

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
+  after_create :send_admin_mail
+  def send_admin_mail
+    AdminMailer.new_user_waiting_for_approval(email).deliver
+  end
 end
