a2c794448e7c756a79fbcf30c40ce3f96bd26ecc
Danniel Tung <dwftung@gmail.com>
2021-04-02 18:38:59 -0300

    specific custom for project

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,17 +2,22 @@ class AdminMailer < ApplicationMailer
   # default from: 'from@example.com'
   # layout 'mailer'
 
-  # def new_user_waiting_for_approval(email)
-  #   @email = email
-  #   mail(to: 'admin@email.com', subject: 'New User Awaiting Admin Approval')
-  # end
-
-  def new_user_waiting_for_approval
-    @user = params[:user] # Instance variable => available in view
+  def new_user_waiting_for_approval(email)
+    # @user = params[:user]
+    @email = email
     mail(
-      to: @user.email,
-      subject: "#{@user.first_name} #{@user.last_name} signed up!"
+      to: 'admin@email.com',
+      # subject: "#{@user.first_name} #{@user.last_name} signed up!"
+      subject: "signed up!"
     )
-    # This will render a view in `app/views/user_mailer`!
   end
+
+  # def new_user_waiting_for_approval
+  #   @user = params[:user] # Instance variable => available in view
+  #   mail(
+  #     to: @user.email,
+  #     subject: "#{@user.first_name} #{@user.last_name} signed up!"
+  #   )
+  #   # This will render a view in `app/views/user_mailer`!
+  # end
 end
