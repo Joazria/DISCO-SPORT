96487227e249686e636c3a80d416eef444ec1432
Danniel Tung <dwftung@gmail.com>
2021-04-03 17:13:35 -0300

    smtp working on development

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :email, presence:true
-  validates :company, presence:true
+  validates :email, presence: true
+  validates :company, presence: true
 
   has_one_attached :avatar
 
