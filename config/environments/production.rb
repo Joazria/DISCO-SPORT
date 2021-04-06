55e67b4bcb8a7b8826e0242ea83ccb8ceeb9793b
Danniel Tung <dwftung@gmail.com>
2021-04-06 14:09:42 -0300

    production refactored

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "https://calm-temple-09020.herokuapp.com" }
   # config.action_mailer.default_url_options = { host: "damp-bastion-50011.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
