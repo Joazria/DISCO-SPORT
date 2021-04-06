78a1b2e49a008432fa184e6b1f9c7ee2236bcdec
JoA Azria <joa.azria@gmail.com>
2021-04-05 22:24:07 -0300

    change smtp env prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "discosport.tv" }
+  config.action_mailer.default_url_options = { host: "smtp.dreamhost.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
