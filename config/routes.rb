d238fa5ca97defd43182753a9ab4017c24f33ebf
Danniel Tung <dwftung@gmail.com>
2021-04-09 18:43:17 -0300

    working on user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-        devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
   root to: 'pages#home'
   get '/about_us', to: 'pages#about_us'
   get '/speaker', to: 'pages#speaker'
   get '/profile', to: 'pages#profile'
   get '/dashboard', to: 'pages#dashboard'
+  resources :users, only: [:show]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
