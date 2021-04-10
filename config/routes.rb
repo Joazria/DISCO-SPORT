4c9119d209e509b0e90e3a8580ebcfaa0b3a26a4
Danniel Tung <dwftung@gmail.com>
2021-04-09 21:48:24 -0300

    user is being sent to profile show view

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/about_us', to: 'pages#about_us'
   get '/speaker', to: 'pages#speaker'
-  get '/profile', to: 'pages#profile'
+  # get '/profile', to: 'pages#profile'
   get '/dashboard', to: 'pages#dashboard'
   resources :profiles, only: [:show]
+  # get '/payments_due/:id', to: 'payments#show_due', as: 'payment_due'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
