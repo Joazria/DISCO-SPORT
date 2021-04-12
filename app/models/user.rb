b8d6d5e4e39e2d37f807c1ebfeb3219357d4e62c
JoA Azria <joa.azria@gmail.com>
2021-04-12 09:47:56 -0300

    ajust

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_one :pitchings
   has_one_attached :avatar
 
-  attribute :status, :string, default: 'Pending'
+  attribute :status, :string, default: 'pending'
 
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
