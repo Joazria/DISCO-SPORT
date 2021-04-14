976d3160c42db5d26f83c69bd9f4ddf65133ad6b
Danniel Tung <dwftung@gmail.com>
2021-04-14 15:47:13 -0300

    update without a view

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # test_user = User.create({company: 'Test',email: 'test1@test.com',password: '123456',country: 'Liberdade',activity: 'Marketing',website: 'www.birds.art.br',gender: 'Mr',first_name: "pat",last_name: "super",phone: "2199999899",whatsapp:"2199999899",linkedin: "https://www.linkedin.com/in/joa-azria/",release: "hello ",status: nil,member: nil,job: nil,category: "pb"})
+
 puts 'Cleaning database...'
 Package.destroy_all if Rails.env.development?
 
-puts 'Creating teddies...'
-Package.create!(price: 1500, sku: 'Add-on #1', name: 'Frontline Visibility')
-Package.create!(price_cents: 749999, sku: 'Add-on #2', name: 'Newsletter')
-Package.create!(price: 7500, sku: 'Add-on #3', name: 'Podcast')
+puts 'Creating Package DiscoSport2021|Africa...'
+Package.create!(price_cents: 150000, sku: 'Add-on #1', name: 'Frontline Visibility', description: 'Frontline Visibility', image:'https://res.cloudinary.com/numama/image/upload/v1616795855/DISCOSPORT/discop_visual_foot_p5cpfx.png')
+Package.create!(price_cents: 750000, sku: 'Add-on #2', name: 'Newsletter', description: 'Branding of the DISCOSPORT newsletter for six months.', image:'https://res.cloudinary.com/numama/image/upload/v1616861877/DISCOSPORT/2_mfwzwq.png')
+Package.create!(price_cents: 750000, sku: 'Add-on #3', name: 'Podcast', description: 'Audio branding of the DISCOSPORT podcast series for six months.', image:'https://res.cloudinary.com/numama/image/upload/v1616861877/DISCOSPORT/3_mzztnv.png')
+Package.create!(price_cents: 750000, sku: 'Add-on #4', name: 'Discosport.tv', description: 'Branding of DISCOSPORT.TV and its main pages for six months.', image:'https://res.cloudinary.com/numama/image/upload/v1616861877/DISCOSPORT/4_zxcr8o.png')
+Package.create!(price_cents: 750000, sku: 'Add-on #5', name: 'Discosport.tv', description: 'Branding of one of on Boot Camp track and greeting opportunities.', image:'https://res.cloudinary.com/numama/image/upload/v1616861877/DISCOSPORT/5_fmhbu0.png')
+Package.create!(price_cents: 750000, sku: 'Add-on #6', name: 'NETWORKING_APP', description: 'Branding of the web and mobile based networking app', image:'https://res.cloudinary.com/numama/image/upload/v1616861877/DISCOSPORT/6_zlopxn.png')
+Package.create!(price_cents: 750000, sku: 'Add-on #7', name: 'NETWORKING_APP', description: 'Virtual Opening Ceremony’s branding and greeting opportunities.', image:'https://res.cloudinary.com/numama/image/upload/v1616861877/DISCOSPORT/7_gs67bq.png')
+Package.create!(price_cents: 750000, sku: 'Add-on #8', name: 'PITCHING', description: 'Pitching Competition’s branding and greeting opportunities.', image:'https://res.cloudinary.com/numama/image/upload/v1618342793/DISCOSPORT/8_rx507s.png')
 
 puts 'Finished!'
