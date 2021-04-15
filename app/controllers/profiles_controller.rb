ab3e9adb1ed26d7b2f5e67aba578f41246dc001a
Danniel Tung <dwftung@gmail.com>
2021-04-15 08:17:58 -0300

    added last_name to searching condition

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -31,7 +31,7 @@ class ProfilesController < ApplicationController
       redirect_to dashboard_path
     else
       @parameter = params[:search].downcase
-      @results = User.all.where("lower(first_name) LIKE :search", search: "%#{@parameter}%")
+      @results = User.all.where("lower(first_name) LIKE :search OR lower(last_name) LIKE :search", search: "%#{@parameter}%")
     end
   end
 
