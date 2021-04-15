cf256a037f7249cf31bc984630fbe5e965428cba
Danniel Tung <dwftung@gmail.com>
2021-04-15 10:18:07 -0300

    refactored

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -31,7 +31,8 @@ class ProfilesController < ApplicationController
       redirect_to dashboard_path
     else
       @parameter = params[:search].downcase
-      @results = User.all.where("lower(first_name) LIKE :search OR lower(last_name) LIKE :search", search: "%#{@parameter}%")
+      # @results = User.all.where("lower(first_name) LIKE :search OR lower(last_name) LIKE :search", search: "%#{@parameter}%")
+      @results = User.all.where("lower(full_name) LIKE :search", search: "%#{@parameter}%")
     end
   end
 
