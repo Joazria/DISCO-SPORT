85edaded235252406f729cf7d731a9bd65748a67
Danniel Tung <dwftung@gmail.com>
2021-04-16 18:59:52 -0300

    refactored chatroom controller

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,9 @@
 class ChatroomsController < ApplicationController
+
+  def create
+    @chatroom = Chatroom.create(name: "#{current_user.first_name}")
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
