786de5a5dfcd4af1b13873e0d4c603a499e3a30f
JoA Azria <joa.azria@gmail.com>
2021-05-03 19:07:25 -0300

    here we fo

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   # validates :company, presence: true
   has_one :pitching
-  has_one :identity
+  has_one :identities, dependent: :destroy
 
   has_one_attached :avatar
   has_many :orders, dependent: :destroy
