7128b9812d7576c49df6e419f6c72ff51d8445e5
JoA Azria <joa.azria@gmail.com>
2021-05-03 11:24:13 -0300

    deleting migration

--- db/migrate/20210503140933_remove_provider_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveProviderFromUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :provider, :string
-    remove_column :users, :uid, :string
-    remove_column :users, :linkedin_picture_url, :string
-    remove_column :users, :token, :string
-    remove_column :users, :token_expiry, :datetime
-  end
-end
