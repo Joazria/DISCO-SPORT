edb9063f87aea572adf8862f7ca3f7d8e078ddad
JoA Azria <joa.azria@gmail.com>
2021-05-31 21:09:23 -0300

    okay

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,14 +1,14 @@
 class Identity < ApplicationRecord
   belongs_to :user
 
-  validates :gender, presence: true
-  validates :job, presence: true
-  validates :company, presence: true
-  validates :country, presence: true
-  validates :website, presence: true
-  validates :phone, presence: true
-  validates :whatsapp, presence: true
-  validates :linkedin, presence: true
+  # validates :gender, presence: true
+  # validates :job, presence: true
+  # validates :company, presence: true
+  # validates :country, presence: true
+  # validates :website, presence: true
+  # validates :phone, presence: true
+  # validates :whatsapp, presence: true
+  # validates :linkedin, presence: true
   validates :release, presence: true
 
 end
